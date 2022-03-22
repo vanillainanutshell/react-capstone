0f4075f0a04dcdb4ba085797e19d7dcfb61738ef
Mauro Vasconcelos <88060989+vanillainanutshell@users.noreply.github.com>
2022-03-22 17:01:13 -0300

    Add linters

---
+++ src/redux/store.js
@@ -0,0 +1,14 @@
+import { applyMiddleware, combineReducers, createStore } from "redux";
+import logger from "redux-logger";
+import thunk from "redux-thunk";
+
+const reducer = combineReducers({
+
+});
+
+const store = createStore(
+    reducer,
+    applyMiddleware(logger, thunk),
+);
+
+export default store;
