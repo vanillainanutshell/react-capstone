5850bd117c03aabfcf33128a5418f7cfafdb6f8f
Mauro Vasconcelos <88060989+vanillainanutshell@users.noreply.github.com>
2022-03-22 16:51:34 -0300

    Setup project

---
+++ src/components/navBar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { NavLink } from 'react-bootstrap';
+
+
+
+
+const NavBar = () =>(
+    <header>
+        <nav>
+            <NavLink to='/'>
+            </NavLink>
+        </nav>
+    </header>
+);
+
+export default NavBar;
